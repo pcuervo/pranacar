0926f28879ca9fbe02e006cbf2e8f6573dd8a170
piers55 <apiers55@gmail.com>
2014-08-06 20:54:34 -0500

    PRANACAR 8:54pm

--- themes/pranacar/header.php
+++ themes/pranacar/header.php
@@ -14,7 +14,7 @@
 	<meta http-equiv="cleartype" content="on">
 	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 
-	<link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic|Roboto:400,100,100italic,300italic,300,900,700italic,500,400italic,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300italic,300,900,700italic,500,400italic,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
@@ -32,30 +32,27 @@
 
 		<header>
 			<div class="clearfix full">
-				<div class="block center columna large-9">
-					<h1 class="columna xmall-6">
+				<div class="block center columna large-10">
+					<h1 class="columna xmall-6 large-2">
 						<a href="index.html">
 							<img src="<?php echo THEMEPATH; ?>images/logo-pranacar-organics-typo.png" alt="">
 						</a>
 					</h1>
 
-					<ul class=" columna xmall-10 large-10">
-						<li>
-							<a href="about.html">Acerca</a>
-						</li>
-						<li >
-							<a class="" href="#historia">Nuestra historia</a>
-						</li>
-						<li>
-							<a href="">¿Qué ofrecemos?</a>
-						</li>
-						<li>
-							<a href="">Nuestras capacidades</a>
-						</li>
-						<li>
-							<a href="">Contacto</a>
-						</li>
-					</ul>
+					<nav class="columna xmall-10 large-8 right">	
+						<a href="about.html">Acerca</a><a class="" href="#historia">
+						Nuestra historia</a><a href="">
+						¿Qué ofrecemos?</a><a href="">
+						Capacidades</a><a href="">
+						Contacto</a>
+					<div class="right"><a href="">
+						Eng</a><a href="">
+						Esp</a>
+					</nav></div>
+						
+					<div>
+					</div>
+					
 				</div>
 			</div>
 		</header>
--- themes/pranacar/index.php
+++ themes/pranacar/index.php
@@ -1,69 +1,77 @@
 <?php get_header(); ?>
 	<section class="clearfix intro"><!--intro-->
-		<div class="width contenido">
-	 		<img class="columna xmall-4 block center" style="margin-left: auto" src="images/logo-pranacar-organics.png" alt="">
+		<div class="width">
+	 		<img class="columna xmall-4 large-2 block center margin.bottom" src="<?php echo THEMEPATH; ?>images/logo-pranacar-organics.png" alt="">
 
-	 	<div class="clear"></div>
+	 		<div class="clear"></div>
 
-	 		<ul class="center block columna xmall-12 cycle-slideshow" data-cycle-fx="fade" data-cycle-timeout="3000" data-cycle-slides="> li">
+	 		<ul class="center block columna xmall-12 cycle-slideshow"
+	 			data-cycle-fx="fade"
+	 			data-cycle-timeout="3000"
+	 			data-cycle-slides="> li"
+	 			data-cycle-auto-height="container"
+	 		>
 
-			        <li class="center block columna xmall-12">Enfocados en el bienestar humano</li>
-
-
-			        <li>Variedad y calidad</li>
-
-			        <li>Eficiencia total en logística y distribución</li>
-
+			        <li class="center block columna xmall-12">Enfocados en el bienestar humano</li>		    
+			    
+			        <li class="center block columna xmall-12">Variedad y calidad</li>
+			    
+			        <li class="center block columna xmall-12">Eficiencia total en logística y distribución</li>
+			    
 			</ul>
 	 	</div>
 
-	 	<div class="clear"></div>
+	 	<div class="clear"></div>	
+	 	
+ 		<div class="center block span xmall-6 medium-10 large-4 sub-menu">
 
- 		<div class="center block span xmall-6 medium-10 large-6 sub-menu">
- 			<div class="columna xmall-12 medium-4 large-3">
+ 			<div class="columna xmall-12 medium-4 large-4">
  				<a class="columna xmall-12 medium-10 large-10" href="">Catálogo</a>
  			</div>
 
- 			<div class="columna xmall-12 medium-4 large-3">
+ 			<div class="columna xmall-12 medium-4 large-4">
  				<a class="columna xmall-12 medium-10 large-10" href="">Contacto</a>
  			</div>
 
- 			<div class="columna xmall-12 medium-4 large-3">
+ 			<div class="columna xmall-12 medium-4 large-4">
  				<a class="columna xmall-12 medium-10 large-10" href="">English</a>
  			</div>
  		</div>
+
 	 </section><!--intro-->
 
 	 <section class="clearfix somos"><!--somos-->
-			<div class="width textos-claro fondo-entero">
-	 			<p class="center block columna xmall-10">Somos una empresa dedicada al comercio internacional de productos enfocados en el bienestar humano.</p>
+			<div class="width textos-claro">
+	 			<h2 class="center block columna xmall-10">Somos una empresa dedicada al comercio internacional de productos enfocados en el bienestar humano.</h2>
 	 		</div>
 
 	 </section><!--somos-->
 
 	 <section class="clearfix acerca"><!--acerca-->
-		<div class="width block center textos-oscuro fondo-entero">
-			<div id="acerca" class=" center block columna xmall-10 margin-bottom">
+		<div class="width block textos-oscuro fondo-entero">
+			<div id="acerca" class="center block text-center columna xmall-10 large-8  margin-bottom">
 				<h2>¿Qué hacemos?</h2>
 		 		<p>Nos especializamos en la importación y exportación de productos orgánicos, naturales y artesanales entre México, Estados Unidos y Canadá. Buscamos ser siempre la mejor solución para los productores, clientes, distribuidores y cadenas comerciales.</p>
 		 	</div>
 
-		 	<div class="center block columna xmall-10 ">
-		 		<h2>Misión</h2>
-		 		<p>Ser la opción de distribución y comercialización de productos saludables, orgánicos y naturales más confiable y eficiente de Centro y Norte América.</p>
-		 	</div>
+		 	<div class="columna large-8 center block">
+			 	<div class="columna xmall-6 text-center">
+			 		<h2>Misión</h2>
+			 		<p>Ser la opción de distribución y comercialización de productos saludables, orgánicos y naturales más confiable y eficiente de Centro y Norte América.</p>
+			 	</div>
 
-		 	<div class="center block columna xmall-10">
-		 		<h2>Visión</h2>
-		 		<p>Dar a nuestros clientes la oportunidad de elevar su bienestar a través de una alimentación y un estilo de vida saludable.</p>
-	 		</div>
+			 	<div class="columna xmall-6 text-center">
+			 		<h2>Visión</h2>
+			 		<p>Dar a nuestros clientes la oportunidad de elevar su bienestar a través de una alimentación y un estilo de vida saludable.</p>
+		 		</div>
+		 	</div>	
 	 	</div>
 	 </section><!--acerca-->
 
 	 <section class="clearfix historia"><!--historia-->
-		<div class="width block center textos-oscuro fondo-entero">
+		<div class="width block textos-oscuro fondo-entero">
 
-			<div id="historia" class=" center block columna xmall-10 margin-bottom sub-menu">
+			<div id="historia" class=" center block columna xmall-8 margin-bottom sub-menu">
 				<h2>Historia</h2>
 
 		 		<p>Nuestro grupo de empresas empezó en el 2004 suministrando productos de necesidad básica para los programas sociales del Gobierno Mexicano. Desde entonces, nuestro grupo se ha convertido en una de las empresas pioneras en la industria de distribución de alimentos.</p>
@@ -72,7 +80,7 @@
 
 		 		<p>Al darnos cuenta de la alta demanda de productos Mexicanos en Estados Unidos y nuestro interés en promover y desarrollar nuestros productos nacionales, Pranacar (una de las divisiones del grupo) se creó en el 2010 para distribuir productos naturales, orgánicos y artesanales en los países de NAFTA con el objetivo principal de llegar a ser una de las principales plataformas de importación y exportación dentro de esta categoría de productos.</p>
 
-		 		<h2 class="center block columna xmall-2 margin-bottom">¿Dudas?</h2>
+		 		<h2 class="text-center margin-bottom">¿Dudas?</h2>
 		 		<a class="center block columna xmall-3 margin-bottom" href="">Contáctanos</a>
 	 		</div>
 	 </section><!--historia-->
@@ -148,9 +156,9 @@
 				</ul>
 			</div>
 		</div>
-	</section><!--productos-->
+	 </section><!--productos-->
 
-	<section class="clearfix contacto"><!--productos-->
+	 <section class="clearfix contacto"><!--productos-->
 		<div class="width block center fondo-entero">
 
 			<div id="contacto" class="columna xmall-6 margin-bottom textos-oscuro">
@@ -173,10 +181,9 @@
 				        <input class="columna xmall-10" type="email" id="email" placeholder="nombre@ejemplo.com" required/>
 
 				        <label class="columna xmall-12" for="msg">Mensaje:</label>
-				        <textarea class="columna xmall-10" id="msg" placeholder="Escribe aquí tu comentario." required></textarea>
-				</fieldset>
-
+				        <textarea class="columna xmall-10" id="msg" placeholder="Escribe aquí tu comentario." required></textarea>    
+				</fieldset>  
 			</div>
 		</div>
-	</section><!--productos-->
+	 </section><!--productos-->
 <?php get_footer(); ?>
